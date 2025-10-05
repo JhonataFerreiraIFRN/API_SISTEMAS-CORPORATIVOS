533bc5f598dd5e1b0753f0f93054189913cfaea0
ericksouza95 <erickagente7@gmail.com>
2025-10-05 17:35:24 -0300

    basicamente todas as tasks da semana

--- app/controllers/movimentacoes_controller.rb
+++ app/controllers/movimentacoes_controller.rb
@@ -3,4 +3,27 @@ class MovimentacoesController < ApplicationController
     movimentacoes = Movimentacao.all
     render json: movimentacoes
   end
+
+  def create
+    movimentacao = Movimentacao.new(movimentacao_params)
+
+    if movimentacao.save
+      render json: movimentacao, status: :created
+    else
+      render json: { errors: movimentacao.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def movimentacao_params
+    params.require(:movimentacao).permit(
+      :tipo_operacao,
+      :correntista_id,
+      :valor_operacao,
+      :data_operacao,
+      :descricao,
+      :correntista_beneficiario_id
+    )
+  end
 end
--- app/models/movimentacao.rb
+++ app/models/movimentacao.rb
@@ -1,2 +1,10 @@
 class Movimentacao < ApplicationRecord
+ #não da pra ficar mais simples que isso mas basicamente: P: Pagamento, T: Transferência S: Saque, D: Depósito
+  VALID_OPERATION_TYPES = %w[P T S D].freeze
+
+  validates :tipo_operacao, presence: true, inclusion: { in: VALID_OPERATION_TYPES }
+  validates :correntista_id, presence: true
+  validates :valor_operacao, presence: true, numericality: { greater_than: 0 }
+  validates :data_operacao, presence: true
+  validates :descricao, presence: true, length: { maximum: 50 }
 end
